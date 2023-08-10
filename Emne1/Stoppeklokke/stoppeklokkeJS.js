2b1a7850b330e8da46ce7d2a27389979103f63b2
S1rShadez <arthure97@hotmail.com>
2023-08-10 14:41:13 +0200

    Created Stoppeklokke

--- Emne1/Stoppeklokke/stoppeklokkeJS.js
+++ Emne1/Stoppeklokke/stoppeklokkeJS.js
@@ -1,9 +1,46 @@
-let timer = 0;
-    
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let timer;
+
 function startStopwatch() {
-    setInterval(countTime, 1000);
+    timer = setInterval(countTime, 1000);
+    if (seconds == 0) {
+        seconds = 1;
+    document.getElementById('tidTeller').innerHTML = hours + "." + minutes + "." + seconds;
+    }
 }
 
-function countTime(){
+function countTime() {
+    if (seconds == 59) {
+        seconds = 0;
+        if (minutes == 59) {
+            minutes = 0;
+            hours++;
+        }
+        else {
+            minutes++;
+        }
+    }
+    else {
+        seconds++;
+    }
+    document.getElementById('tidTeller').innerHTML = hours + "." + minutes + "." + seconds;
+}
 
+function saveLap() {
+    document.getElementById('savedLaps').innerHTML += hours + "." + minutes + "." + seconds + "<br>";
+}
+
+function stopTimer(){
+    clearInterval(timer);
+}
+
+function resetAll() {
+    clearInterval(timer);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById('tidTeller').innerHTML = "0.0.0";
+    document.getElementById('savedLaps').innerHTML = "";
 }
